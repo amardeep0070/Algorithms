ed32ae51a667df75ca3652701b36303bc092b9c3
Amardeep Singh <amardeep@ccs.neu.edu>
2016-10-04 18:28:56 -0400

    Update Solution.java

--- ImageRotation/src/Solution.java
+++ ImageRotation/src/Solution.java
@@ -1,29 +1,24 @@
 public class Solution {
-    public void rotate(int[][] matrix) {
-            int[][] result = new int[matrix.length][matrix.length];
-            int k=0;
-            for (int i=matrix.length-1; i>=0; i--){
-            	for(int j=0; j<=matrix.length-1;j++){
-            		result[j][k]=matrix[i][j];
-            	}
-            	k++;
-            }
-            for (int i=0; i<matrix.length;i++){
-    			for (int j=0; j<matrix[0].length;j++){
-    				System.out.println(result[i][j]);
-    			}
-    }
-    }
-            
-    public static void main(String[] args) {
+	public void rotate(int[][] matrix) {
+		int lengthMatrix=matrix.length;
+		int[][] result = new int[lengthMatrix][lengthMatrix];
+		int k=0;
+		for (int i=lengthMatrix-1; i>=0; i--){
+			for(int j=0; j<=lengthMatrix-1;j++){
+				result[j][k]=matrix[i][j];
+			}
+			k++;
+		}
+		
+		for (int i=0; i<lengthMatrix;i++){
+			for (int j=0; j<lengthMatrix;j++){
+				matrix[i][j]=result[i][j];
+			}
+		}
+	}
+	public static void main(String[] args) {
 		Solution s = new  Solution();
 		int[][] matrix = new int[][]{{1,2},{3,4}};
 		s.rotate(matrix);
-//		//System.out.println("m is" + matrix[1][1]);
-//		for (int i=0; i<matrix.length;i++){
-//			for (int j=0; j<matrix.length;j++){
-//				System.out.println(matrix[i][j]);
-//			}
-//		}
-   }
 	}
+}
